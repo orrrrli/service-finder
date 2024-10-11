31c6f5ec8b89738b5e1604af2fcf6f66321dc99d
Orlando Castañeda Sanchez <castaneda.kevin@uabc.edu.mx>
2024-10-10 23:43:23 -0700

    update: add auth to project

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,12 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "servicesfinder0e44342d0e44342d": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
